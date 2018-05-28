034f5fc4c46dacf140bc831500ae96693c055132
wangkaixuan <wangkaixuanjc@gmail.com>
2018-05-28 22:14:41 +0800

    Study Custom View

--- app/src/main/java/com/sample/studycustomview/MainActivity.java
+++ app/src/main/java/com/sample/studycustomview/MainActivity.java
@@ -2,11 +2,8 @@ package com.sample.studycustomview;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private CustomTitleView titleBar;
-    private TextView test;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
